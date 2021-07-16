cb53866000cb8191fdbfe4d0e23a683a2dfc76f5
khalidabdulrahman <87329986+khalidabdulrahman@users.noreply.github.com>
2021-07-16 15:23:55 +0300

    n

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,10 +8,119 @@ import { Observable } from 'rxjs';
 import 'firebase/firestore';
 import * as firebase from 'firebase/app';
 
+
+
+
+export interface User {
+  id?:string,
+  email:string,
+  name: string,
+  mobile:number,
+  isAdmin:boolean,
+
+}
+export interface Order{
+  customerid?:any;
+  type?:any;
+  status?: any,
+  date?:any,
+  address?:any,
+ 
+
+
+}
+export interface Vehicle{
+  customerid?:any;
+  company?:any;
+  series?: any,
+  year?:any,
+  plateno?:any,
+
+}
+
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  users:Observable<User[]>;
+  usersCollectionRef:AngularFirestoreCollection<User>;
 
-  constructor(public afAuth: AngularFireAuth, afs: AngularFirestore) { }
+  orders:Observable<Order[]>;
+  ordersCollectionRef:AngularFirestoreCollection<Order>;
+
+  vehicles:Observable<Vehicle[]>;
+  vehiclesCollectionRef:AngularFirestoreCollection<Vehicle>;
+  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) { 
+    this.usersCollectionRef = this.afs.collection('Users'); 
+       // this.products = this.productsCollectionRef.valueChanges();
+        this.users=this.usersCollectionRef.snapshotChanges().pipe(
+    
+          map(actions => {
+    
+            return actions.map(a => {
+    
+              const data = a.payload.doc.data();
+    
+              const id = a.payload.doc.id;
+    
+              return { id, ...data };
+    
+            });
+    
+          })
+        );
+
+        this.ordersCollectionRef = this.afs.collection('Orders'); 
+       // this.products = this.productsCollectionRef.valueChanges();
+        this.orders=this.ordersCollectionRef.snapshotChanges().pipe(
+    
+          map(actions => {
+    
+            return actions.map(a => {
+    
+              const data = a.payload.doc.data();
+    
+              const id = a.payload.doc.id;
+    
+              return { id, ...data };
+    
+            });
+    
+          })
+    
+        );
+    
+
+        this.vehiclesCollectionRef = this.afs.collection('Vehicles'); 
+       // this.products = this.productsCollectionRef.valueChanges();
+        this.vehicles=this.vehiclesCollectionRef.snapshotChanges().pipe(
+    
+          map(actions => {
+    
+            return actions.map(a => {
+    
+              const data = a.payload.doc.data();
+    
+              const id = a.payload.doc.id;
+    
+              return { id, ...data };
+    
+            });
+    
+          })
+    
+        );
+
+  }
+
+   
+
+
+
+
+
+
 }
