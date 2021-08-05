23857e58d7a1dcb177bc05b14675d6ec34e4d9d0
khalidabdulrahman <87329986+khalidabdulrahman@users.noreply.github.com>
2021-08-05 17:36:46 +0300

    df

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -16,22 +16,46 @@ public id;
 public sub;
 public array:any[]=[];
 public x;
-public profit=0 ;
+public profit:number=0 ;
+public net:number=0;
+public tax:number;
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id']; 
     
   });
+
+  this.afs.collection("Orders")
+  .get().subscribe((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          this.array.push(doc.data())
+       
+        
+      });
+    this.array.forEach(element => {
+      if(element.status=="completed"){
+        if(element.providerid==this.id){
+      this.profit+=element.price
+      }}
+    });
+this.tax=this.profit*0.15;    
+this.net=this.profit-this.tax;
+  })
 }
 
 
 
 details(id,prov){
-
     this.router.navigate(['/paymentdetails/'+id+'/'+prov])
 }
-}
 
+
+calculateprofit(){
+  
+}
+}
 
 
 
